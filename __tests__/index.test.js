0a083c3e3cef0766feec0df16b68233562c6fcba
Byron McMullen <byronsm@gmail.com>
2018-01-24 19:46:59 -0800

    Prettier Formatting

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,14 +1,14 @@
-import Builder, { OptionLiterals, InvalidField, InvalidChoice } from 'builder';
+import Builder, { OptionLiterals, InvalidField, InvalidChoice } from "builder";
 
 // example of a class configuration (post-compile)
 const testDefinition = {
-  name: '13th Age',
+  name: "13th Age",
   fields: {
     name: { options: OptionLiterals.STRING, required: true },
     level: {
-      options: OptionLiterals.NUMBER, 
-      validation: (v) => (v >= 1 && v <= 10), 
-      required: true 
+      options: OptionLiterals.NUMBER,
+      validation: v => v >= 1 && v <= 10,
+      required: true
     },
     hometown: { options: OptionLiterals.STRING },
     class: {
@@ -19,17 +19,17 @@ const testDefinition = {
             handed: {
               options: {
                 one_handed: {
-                  options: { small: {}, medium: {} },
+                  options: { small: {}, medium: {} }
                 },
-                two_handed: {},
+                two_handed: {}
               }
             }
           }
         },
-        barbarian: {},
+        barbarian: {}
       }
     }
-  },
+  }
 };
 
 function testCharBuilder(startValues = {}) {
@@ -37,98 +37,137 @@ function testCharBuilder(startValues = {}) {
   return new Creator(startValues);
 }
 
-describe('creating a builder', () => {
-  describe('constructor', () => {
-    it('can set multiple fields from a config param', () => {
-      const character = testCharBuilder({ name: 'Ragnar', hometown: 'Neverwinter', class: 'fighter' });
-      expect(character.get('name')).toEqual('Ragnar');
-      expect(character.get('hometown')).toEqual('Neverwinter');
-      expect(character.get('class')).toEqual('fighter');
+describe("creating a builder", () => {
+  describe("constructor", () => {
+    it("can set multiple fields from a config param", () => {
+      const character = testCharBuilder({
+        name: "Ragnar",
+        hometown: "Neverwinter",
+        class: "fighter"
+      });
+      expect(character.get("name")).toEqual("Ragnar");
+      expect(character.get("hometown")).toEqual("Neverwinter");
+      expect(character.get("class")).toEqual("fighter");
     });
-    it('throws if given values for undefined fields', () => {
-      expect(() => testCharBuilder({ favoriteColor: 'red' })).toThrowError(InvalidField);
+    it("can set options introduced by earlier options", () => {
+      const character = testCharBuilder({
+        class: "fighter",
+        "class.ability": "str"
+      });
+      expect(character.get("class")).toEqual("fighter");
+      expect(character.get("class.ability")).toEqual("str");
     });
-    it('throws if given values of incorrect type to a field', () => {
+    it("throws if given values for undefined fields", () => {
+      expect(() => testCharBuilder({ favoriteColor: "red" })).toThrowError(
+        InvalidField
+      );
+    });
+    it("throws if given values for a nested choice, when the parent choice isn't made yet", () => {
+      expect(() => testCharBuilder({ "class.ability": "str" })).toThrowError(
+        InvalidField
+      );
+    });
+    it("throws if given values of incorrect type to a field", () => {
       expect(() => testCharBuilder({ name: 1 })).toThrowError(InvalidChoice);
-      expect(() => testCharBuilder({ level: 'one' })).toThrowError(InvalidChoice);
+      expect(() => testCharBuilder({ level: "one" })).toThrowError(
+        InvalidChoice
+      );
     });
-    it('throws if given a value that violates field\'s validation rule', () => {
+    it("throws if given a value that violates field's validation rule", () => {
       expect(() => testCharBuilder({ level: 30 })).toThrowError(InvalidChoice);
     });
-    it('throw if given a value that is not among the list of options', () => {
-      expect(() => testCharBuilder({ class: 'accountant' })).toThrowError(InvalidChoice);
+    it("throw if given a value that is not among the list of options", () => {
+      expect(() => testCharBuilder({ class: "accountant" })).toThrowError(
+        InvalidChoice
+      );
     });
-    it('imposes modifications defined by the option choice');
+    it("imposes modifications defined by the option choice");
   });
-  describe('.fields()', () => {
-    it('can list all fields, even once chosen', () => {
-      const character = testCharBuilder().choose('name', 'Ragnar');
-      expect(character.fields()).toEqual(expect.arrayContaining(['name', 'level']));
+  describe(".fields()", () => {
+    it("can list all fields, even once chosen", () => {
+      const character = testCharBuilder().choose("name", "Ragnar");
+      expect(character.fields()).toEqual(
+        expect.arrayContaining(["name", "level"])
+      );
     });
-    it('can list all fields, even ones added by a choice', () => {
-      const character = testCharBuilder().choose('class', 'fighter');
-      expect(character.fields()).toEqual(expect.arrayContaining(['name', 'class.ability']));
+    it("can list all fields, even ones added by a choice", () => {
+      const character = testCharBuilder().choose("class", "fighter");
+      expect(character.fields()).toEqual(
+        expect.arrayContaining(["name", "class.ability"])
+      );
     });
   });
-  it('can list required fields', () => {
+  it("can list required fields", () => {
     const required = testCharBuilder().requires();
-    const expected = expect.arrayContaining(['name', 'level']);
-    const unexpected = expect.arrayContaining(['hometown']);
+    const expected = expect.arrayContaining(["name", "level"]);
+    const unexpected = expect.arrayContaining(["hometown"]);
     expect(required).toEqual(expected);
-    expect(required).not.toEqual(unexpected)
+    expect(required).not.toEqual(unexpected);
   });
-  it('lists possible fields', () => {
+  it("lists possible fields", () => {
     const missing = testCharBuilder().missing();
-    const expected = expect.arrayContaining(['name', 'level', 'hometown']);
+    const expected = expect.arrayContaining(["name", "level", "hometown"]);
     expect(missing).toEqual(expected);
   });
-  describe('.options([fieldName])', () => {
-    it('returns valid options for literal fields', () => {
-      const options = testCharBuilder().options('name');
+  describe(".options([fieldName])", () => {
+    it("returns valid options for literal fields", () => {
+      const options = testCharBuilder().options("name");
       const expected = OptionLiterals.STRING;
       expect(options).toEqual(expected);
     });
 
-    it('returns the options for custom fields', () => {
-      const options = testCharBuilder().options('class');
-      const expected = expect.arrayContaining(['fighter', 'barbarian']);
-    })
+    it("returns the options for custom fields", () => {
+      const options = testCharBuilder().options("class");
+      const expected = expect.arrayContaining(["fighter", "barbarian"]);
+    });
   });
 
-  describe('.choose([fieldName], [value])', () => {
-    it('creates a new character', () => {
+  describe(".choose([fieldName], [value])", () => {
+    it("creates a new character", () => {
       const character = testCharBuilder();
-      const nextCharacter = character.choose('name', 'Ragnar');
+      const nextCharacter = character.choose("name", "Ragnar");
       expect(nextCharacter).not.toBe(character);
     });
-    it('returns a character with the field set', () => {
-      const nextCharacter = testCharBuilder().choose('name', 'Ragnar');
-      expect(nextCharacter.get('name')).toEqual('Ragnar');
+    it("returns a character with the field set", () => {
+      const nextCharacter = testCharBuilder().choose("name", "Ragnar");
+      expect(nextCharacter.get("name")).toEqual("Ragnar");
     });
-    it('removes choice from list of possible fields', () => {
-      const missing = testCharBuilder().choose('name', 'Ragnar').missing();
-      const expected = expect.arrayContaining(['level', 'hometown']);
-      const unexpected = expect.arrayContaining(['name']);
+    it("removes choice from list of possible fields", () => {
+      const missing = testCharBuilder()
+        .choose("name", "Ragnar")
+        .missing();
+      const expected = expect.arrayContaining(["level", "hometown"]);
+      const unexpected = expect.arrayContaining(["name"]);
       expect(missing).toEqual(expected);
       expect(missing).not.toEqual(unexpected);
     });
-    it('can be chained', () => {
-      const character = testCharBuilder().choose('name', 'ragnar').choose('level', 3);
-      expect(character.get('name')).toEqual('ragnar');
-      expect(character.get('level')).toEqual(3);
+    it("can be chained", () => {
+      const character = testCharBuilder()
+        .choose("name", "ragnar")
+        .choose("level", 3);
+      expect(character.get("name")).toEqual("ragnar");
+      expect(character.get("level")).toEqual(3);
     });
-    it('can nest', () => {
-      const character = testCharBuilder().choose('class', 'fighter').choose('class.weapon', 'one_handed');
-      expect(character.get('class')).toEqual('fighter');
-      expect(character.get('class.weapon')).toEqual('one_handed');
+    it("can nest", () => {
+      const character = testCharBuilder()
+        .choose("class", "fighter")
+        .choose("class.weapon", "one_handed");
+      expect(character.get("class")).toEqual("fighter");
+      expect(character.get("class.weapon")).toEqual("one_handed");
     });
-    it('can add new choices', () => {
-      const character = testCharBuilder().choose('class', 'fighter');
-      expect(character.missing()).toEqual(expect.arrayContaining(['class.ability']));
+    it("can add new choices", () => {
+      const character = testCharBuilder().choose("class", "fighter");
+      expect(character.missing()).toEqual(
+        expect.arrayContaining(["class.ability"])
+      );
     });
-    xit('can add new choices that nest', () => {
-      const character = testCharBuilder().choose('class', 'fighter').choose('class.weapon', 'one_handed');
-      expect(character.missing()).toEqual(expect.arrayContaining(['class.weapon.handed']));
+    xit("can add new choices that nest", () => {
+      const character = testCharBuilder()
+        .choose("class", "fighter")
+        .choose("class.weapon", "one_handed");
+      expect(character.missing()).toEqual(
+        expect.arrayContaining(["class.weapon.handed"])
+      );
     });
   });
 });
--- src/builder.js
+++ src/builder.js
@@ -1,4 +1,29 @@
-import { append, filter, propEq, keys, pick, dropLast, contains, not, equals, always, compose, join, merge, values, difference, forEach, concat, map, flatten, path, split, last, intersperse, __ } from 'ramda';
+import {
+  append,
+  filter,
+  propEq,
+  keys,
+  pick,
+  dropLast,
+  contains,
+  not,
+  equals,
+  always,
+  compose,
+  join,
+  merge,
+  values,
+  difference,
+  forEach,
+  concat,
+  map,
+  flatten,
+  path,
+  split,
+  last,
+  intersperse,
+  __
+} from "ramda";
 
 export class InvalidField extends Error {
   constructor(field, ...params) {
@@ -29,41 +54,57 @@ export class InvalidChoice extends Error {
 
 export default function(definition) {
   // choices that have custom side effects
-  const nonLiteral = (f) => not(contains(f.options, values(OptionLiterals)));
+  const nonLiteral = f => not(contains(f.options, values(OptionLiterals)));
   const nonLiteralDefChoices = keys(filter(nonLiteral, definition.fields));
   return class GeneratedBuilder {
-    
     constructor(values) {
       this.choices = {};
 
       function set(field, value) {
         this.choices[field] = value;
       }
-      
+
       const validFields = keys(definition.fields);
-      
-      Object.keys(values).forEach((field) => {
+
+      Object.keys(values).forEach(field => {
         const nonLiteralChoices = nonLiteralDefChoices;
         if (not(contains(field, this.fields()))) throw new InvalidField(field);
-        const choices = split('.', field);
-        const choicePath =  intersperse('options', choices); // append(last(choices), flatten(map(v => [v, 'options'], dropLast(choices))));
-        const validOptions = path(append('options', choicePath), definition.fields);
+        const choices = split(".", field);
+        const choicePath = intersperse("options", choices); // append(last(choices), flatten(map(v => [v, 'options'], dropLast(choices))));
+        const validOptions = path(
+          append("options", choicePath),
+          definition.fields
+        );
         switch (typeof validOptions) {
-          case 'symbol': {
-            const notMatchType = compose(not, equals(__, (typeof values[field])));
+          case "symbol": {
+            const notMatchType = compose(not, equals(__, typeof values[field]));
             switch (validOptions) {
               case OptionLiterals.STRING:
-                if (notMatchType('string')) throw new InvalidChoice(field, values[field], 'must be string');
+                if (notMatchType("string"))
+                  throw new InvalidChoice(
+                    field,
+                    values[field],
+                    "must be string"
+                  );
                 break;
               case OptionLiterals.NUMBER:
-                if (notMatchType('number')) throw new InvalidChoice(field, values[field], 'must be number');
+                if (notMatchType("number"))
+                  throw new InvalidChoice(
+                    field,
+                    values[field],
+                    "must be number"
+                  );
                 break;
             }
             break;
           }
-          case 'object': {
+          case "object": {
             if (not(contains(values[field], keys(validOptions)))) {
-              throw new InvalidChoice(field, values[field], `must be one of [${join(', ', keys(validOptions))}]`);
+              throw new InvalidChoice(
+                field,
+                values[field],
+                `must be one of [${join(", ", keys(validOptions))}]`
+              );
               // TODO: I think what I want to do is: when an enumerated option with modifications is chosen it creates a new GeneratedBuilder class using the higher
               // order function. That GeneratedBuilder retains a back link to "this" instance and merges its own options with the ones downstream".
             }
@@ -72,8 +113,11 @@ export default function(definition) {
             break;
         }
         // if we don't have a validation rule, then it is always valid
-        const validationRule = path(append('validation', choicePath), definition.fields) || always(true);
-        if (not(validationRule(values[field]))) throw new InvalidChoice(field, values[field], )
+        const validationRule =
+          path(append("validation", choicePath), definition.fields) ||
+          always(true);
+        if (not(validationRule(values[field])))
+          throw new InvalidChoice(field, values[field]);
         set.call(this, field, values[field]);
       });
     }
@@ -83,51 +127,62 @@ export default function(definition) {
     }
 
     fields() {
-      const root = keys(definition.fields)
-      return concat(root, flatten(map((f) => {
-        const nestedKey = i => `${f}.${i}`;
-        return map(nestedKey, keys(definition.fields[f].options[this.choices[f]]));
-      }, root)));
+      const root = keys(definition.fields);
+      return concat(
+        root,
+        flatten(
+          map(f => {
+            const nestedKey = i => `${f}.${i}`;
+            return map(
+              nestedKey,
+              keys(definition.fields[f].options[this.choices[f]])
+            );
+          }, root)
+        )
+      );
     }
 
     requires() {
-      return keys(filter(propEq('required', true), definition.fields));
+      return keys(filter(propEq("required", true), definition.fields));
     }
 
     missing() {
       let result = [];
-      const undefinedChoice = (field) => (this.choices[field] === undefined);
+      const undefinedChoice = field => this.choices[field] === undefined;
       // fields missing values
       const rootMissing = filter(undefinedChoice, keys(definition.fields));
       // non-literal fields with values
       const nextLevel = difference(nonLiteralDefChoices, rootMissing);
-      const nestedMissing = flatten(map((f) => {
-        const innerFields = definition.fields[f].options[this.choices[f]];
-        const nestedKey = i => `${f}.${i}`;
-        const innerKeys = map(nestedKey, keys(innerFields));
-        return filter(undefinedChoice, innerKeys);
-      }, nextLevel));
+      const nestedMissing = flatten(
+        map(f => {
+          const innerFields = definition.fields[f].options[this.choices[f]];
+          const nestedKey = i => `${f}.${i}`;
+          const innerKeys = map(nestedKey, keys(innerFields));
+          return filter(undefinedChoice, innerKeys);
+        }, nextLevel)
+      );
       return concat(rootMissing, nestedMissing);
     }
 
     options(field) {
-
       return definition.fields[field].options;
     }
 
     choose(field, value) {
-      return new GeneratedBuilder.prototype.constructor(merge(this.choices, { [field]: value }));
+      return new GeneratedBuilder.prototype.constructor(
+        merge(this.choices, { [field]: value })
+      );
     }
 
     get(field) {
       return this.choices[field];
     }
-  }
+  };
 }
 
-const STRING = Symbol('STRING');
-const NUMBER = Symbol('NUMBER');
+const STRING = Symbol("STRING");
+const NUMBER = Symbol("NUMBER");
 export const OptionLiterals = {
   STRING: STRING,
-  NUMBER: NUMBER,
+  NUMBER: NUMBER
 };
